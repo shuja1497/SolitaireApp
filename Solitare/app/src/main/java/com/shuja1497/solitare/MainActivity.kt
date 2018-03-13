f8f5e8d714eab85273d762ca76a586354dea61c0
shuja1497 <shujareshi1497@gmail.com>
2018-03-13 19:02:01 +0530

    creating counter app using Anko

--- Solitare/app/src/main/java/com/shuja1497/solitare/MainActivity.kt
+++ Solitare/app/src/main/java/com/shuja1497/solitare/MainActivity.kt
@@ -2,10 +2,30 @@ package com.shuja1497.solitare
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import org.jetbrains.anko.button
+import org.jetbrains.anko.onClick
+import org.jetbrains.anko.relativeLayout
+import org.jetbrains.anko.textView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        var count = 0
+        relativeLayout {
+
+            val counterTextView = textView {
+                text = "0"
+                textSize = 24f // in anko text size is given as float which is same as SP
+            }
+
+            button {
+                onClick {
+                    count++
+                    counterTextView.text = count.toString()
+                }
+            }
+        }
     }
 }
