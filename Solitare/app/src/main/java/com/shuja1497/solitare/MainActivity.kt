003baad7a26663e774d98a580e3163c65df7dcc9
shuja1497 <shujareshi1497@gmail.com>
2018-03-14 00:47:38 +0530

    setting up the layout with card width and card height.

--- Solitare/app/src/main/java/com/shuja1497/solitare/MainActivity.kt
+++ Solitare/app/src/main/java/com/shuja1497/solitare/MainActivity.kt
@@ -10,6 +10,22 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        // leaving 4dp on left and right and having 7 cards .
+        val cardWidth = (displayMetrics.widthPixels - dip(8))/7
+        val cardHeight = cardWidth * (190/140) // must be in same ratio as in the resource folder.
+
+        verticalLayout {
+            //linear layout with vertical orientation
+
+            linearLayout {
+
+            }
+
+            linearLayout {
+
+            }
+        }
+
 
     }
 }
