dd526bec0709b54764681836cf5a0478b4bccaa9
shuja1497 <shujareshi1497@gmail.com>
2018-03-14 01:08:06 +0530

    Finishing the first row of the layout.

--- Solitare/app/src/main/java/com/shuja1497/solitare/MainActivity.kt
+++ Solitare/app/src/main/java/com/shuja1497/solitare/MainActivity.kt
@@ -14,13 +14,28 @@ class MainActivity : AppCompatActivity() {
         val cardWidth = (displayMetrics.widthPixels - dip(8))/7
         val cardHeight = cardWidth * (190/140) // must be in same ratio as in the resource folder.
 
+        //linear layout with vertical orientation
         verticalLayout {
-            //linear layout with vertical orientation
+            leftPadding = dip(4)
+            rightPadding = dip(4)
+            topPadding = dip(8)
 
             linearLayout {
+//                imageView {
+//                    imageResource = R.drawable.cardback_green1
+//                }.lparams {
+//                    width = cardWidth
+//                    height = cardHeight
+//                }
 
+                imageView(imageResource=R.drawable.cardback_green5).lparams(cardWidth, cardHeight)// deck
+                imageView(imageResource=R.drawable.cardback_blue1).lparams(cardWidth, cardHeight)// waste pile
+                view().lparams(cardWidth, 0) // for a space btwn waste pile and foundation pile
+
+                for (i in 0..3){
+                    imageView(imageResource=R.drawable.cardback_blue1).lparams(cardWidth, cardHeight)// waste pile
+                }
             }
-
             linearLayout {
 
             }
